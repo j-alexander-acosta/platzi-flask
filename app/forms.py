89fd8402e50196bf350ae6d6476e2b843e8a1f40
Alexander_Acosta <alexanderacosta@unach.cl>
2021-10-10 21:18:30 -0300

    Finalización del curso

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,21 @@
+from inspect import ClosureVars
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
-class loginForm(FlaskForm):
+class LoginForm(FlaskForm):
     username = StringField('Nombre de Usuario', validators=[DataRequired()])
     password = PasswordField('Contraseña', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
+
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
+
+class UpdateTodoForm(FlaskForm):
+    submit = SubmitField('Actualizar')
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import request, make_response, redirect, render_template, session, url_for, flash
 import unittest
+from flask import request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
+
 
 from app import create_app
-from app.forms import loginForm
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import update_todo, get_todos, put_todo, delete_todo
 
 app = create_app()
-
-
-todos = ['Comprar café', 'Enviar solicitud de compra', 'Entregar video al productor']
 
 
 @app.cli.command()
@@ -16,13 +16,14 @@ def test():
     unittest.TextTestRunner().run(tests)
 
 
-@app.errorhandler(500)
-def server_error(e):
-    return render_template('500.html'), 500
-
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return render_template('500.html', error=error)
 
 
 @app.route('/')
@@ -31,28 +32,50 @@ def index():
 
     response = make_response(redirect('/hello'))
     session['user_ip'] = user_ip
+
     return response
 
 
 @app.route('/hello', methods=['GET', 'POST'])
+@login_required
 def hello():
     user_ip = session.get('user_ip')
-    login_form = loginForm()
-    username = session.get('username')
+    username = current_user.id
+    todo_form = TodoForm()
+    delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
-        'user_ip' : user_ip,
-        'todos' : todos,
-        'login_form' : login_form,
-        'username': username
+        'user_ip': user_ip,
+        'todos': get_todos(user_id=username),
+        'username': username,
+        'todo_form': todo_form,
+        'delete_form': delete_form,
+        'update_form': update_form
     }
 
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
 
-        flash('Nombre de usuario registrado con éxito!')
+        flash('Tu tarea se creo con éxito!')
 
-        return redirect(url_for('index'))
+        return redirect(url_for('hello'))
 
     return render_template('hello.html', **context)
+
+
+@app.route('/todos/delete/<todo_id>', methods=['POST'])
+def delete(todo_id):
+    user_id = current_user.id
+    delete_todo(user_id=user_id, todo_id=todo_id)
+
+    return redirect(url_for('hello'))
+
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+
+    update_todo(user_id=user_id, todo_id=todo_id, done=done)
+
+    return redirect(url_for('hello'))
